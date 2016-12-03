1a6da13ff6d3bd4a11e2c7b7253c4abd04b82c57
babak soltanifar <jupitermaster83@yahoo.com>
2016-12-03 01:17:57 -0500

    node js using mlab

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 /**
  * http://usejsdoc.org/
  */
-var express=require("express");
-var todocontroller=require("./controllers/todocontroller");
-var app=express();
-//setup template
+var express = require("express");
+var mongos = require("mongoose");
+var todocontroller = require("./controllers/todocontroller");
+var app = express();
+// connect to db
+// setup template
+//mongos.connect('mongodb://user1:user1@ds050879.mlab.com:50879/todo');
+//var todoSchema = new mongos.Schema({
+//	item : String
+//});
+//var Todo = mongos.model("Todo", todoSchema);
+//var itemOne = Todo({
+//	item : "get Flowers"
+//}).save(function(err) {
+//	if (err) {
+//		throw err;
+//	}
+//});
 app.set('view engine', 'ejs');
 app.use(express.static("./public"));
-//app.use("/assets",express.static("./public/assets"));
+// app.use("/assets",express.static("./public/assets"));
 // fire the controller
 todocontroller(app);
-
-
 
 app.listen(3000);
--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -2,20 +2,53 @@
  * http://usejsdoc.org/
  */
 var bodyParser=require("body-parser");
+var mongos = require("mongoose");
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 module.exports=function(app){
-  var data=[{item:"get milk"},{item:"get bread"},{item:"get butter"}];
+//  var data=[{item:"get milk"},{item:"get bread"},{item:"get butter"}];
+	mongos.connect('mongodb://user1:user1@ds050879.mlab.com:50879/todo');
+	var todoSchema = new mongos.Schema({
+		item : String
+	});
+	var Todo = mongos.model("Todo", todoSchema);
+//	var itemOne = Todo({
+//		item : "get Flowers"
+//	}).save(function(err) {
+//		if (err) {
+//			throw err;
+//		}
+//	});
 	app.get("/todo",function(req,res){
 		console.log("inside"+req.url);
 	//	res.render("todo");
-		res.render("todo",{todos:data});
+		/*
+		 * get data from mongo and pass it to view
+		 * 
+		 */
+		Todo.find({},function(err,data){
+			if(err){
+				throw err;
+			}else{
+				res.render("todo",{todos:data});
+			}
+		});
+		
+	//	res.render("todo",{todos:data});
 	});
 	app.post("/todo",urlencodedParser, function (req, res) {
-		console.log("post"+req.body);
-		data.push(req.body);
+//		console.log("post"+req.body);
+//		data.push(req.body);
 	//	res.json(data);
-		res.render("todo",{todos:data});
+//		res.render("todo",{todos:data});
 	//	res.JSON(data);
+		/**************************************************************/
+		var newTodo=Todo(req.body).save(function(err,data){
+			if(err){
+				throw err;
+			}
+				res.render("todo",{todos:data});
+			
+				});
 		
 		
 	});
